952c857860c026990a45065d31766281313cda66
SkelSec <info@skelsec.com>
2020-03-09 18:27:57 +0100

    version bump

--- msldap/_version.py
+++ msldap/_version.py
@@ -1,7 +1,7 @@
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 __banner__ = \
 """
 # msldap %s 
-# Author: Tamas Jos @skelsec (skelsecprojects@gmail.com)
+# Author: Tamas Jos @skelsec (info@skelsecprojects.com)
 """ % __version__
