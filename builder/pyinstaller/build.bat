ca427429180842df5860c6d8981d4b1fccd30189
SkelSec <skelsec@skelsecprojects.com>
2023-02-21 09:22:05 -0800

    final builder

--- builder/pyinstaller/build.bat
+++ builder/pyinstaller/build.bat
@@ -1,14 +1,26 @@
 @echo off
+set projectname=msldap
 set hiddenimports= --hidden-import cryptography --hidden-import cffi --hidden-import cryptography.hazmat.backends.openssl --hidden-import cryptography.hazmat.bindings._openssl --hidden-import unicrypto --hidden-import unicrypto.backends.pycryptodome.DES --hidden-import  unicrypto.backends.pycryptodome.TDES --hidden-import unicrypto.backends.pycryptodome.AES --hidden-import unicrypto.backends.pycryptodome.RC4 --hidden-import unicrypto.backends.pure.DES --hidden-import  unicrypto.backends.pure.TDES --hidden-import unicrypto.backends.pure.AES --hidden-import unicrypto.backends.pure.RC4 --hidden-import unicrypto.backends.cryptography.DES --hidden-import  unicrypto.backends.cryptography.TDES --hidden-import unicrypto.backends.cryptography.AES --hidden-import unicrypto.backends.cryptography.RC4 --hidden-import unicrypto.backends.pycryptodomex.DES --hidden-import  unicrypto.backends.pycryptodomex.TDES --hidden-import unicrypto.backends.pycryptodomex.AES --hidden-import unicrypto.backends.pycryptodomex.RC4
 set root=%~dp0
-set projectname=msldap
-set pyenv=%root%\env
 set repo=%root%..\..\%projectname%
-python -m venv %pyenv%
-%pyenv%\Scripts\activate.bat &^
-pip install pyinstaller &^
-cd %repo%\..\ &^
-pip install . &^
-cd %repo%\examples &^
-pyinstaller -F msldapclient.py %hiddenimports% &^
+IF NOT DEFINED __BUILDALL_VENV__ (GOTO :CREATEVENV)
+GOTO :BUILD
+
+:CREATEVENV
+python -m venv %root%\env
+CALL %root%\env\Scripts\activate.bat
+pip install pyinstaller
+GOTO :BUILD
+
+:BUILD
+cd %repo%\..\
+pip install .
+cd %repo%\examples
+pyinstaller -F msldapclient.py %hiddenimports%
 cd %repo%\examples\dist & copy *.exe %root%
+GOTO :CLEANUP
+
+:CLEANUP
+IF NOT DEFINED __BUILDALL_VENV__ (deactivate)
+cd %root%
+EXIT /B
